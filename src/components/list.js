3b711b86f007c3371101245b1371adbb6bb34869
vencax <vencax@centrum.cz>
2021-09-23 12:19:25 +0200

    list tunning

--- src/components/list.js
+++ src/components/list.js
@@ -1,17 +1,27 @@
 import Actions from './actions.js'
+import { priority, state } from './filters.js'
 
+const WARN_THRESHOLD = 5 * 24 * 60 * 60 * 1000  // 5 dayss
 const prioClasses = {
   'low': null,
   'nor': 'table-success',
   'hi': 'table-warning',
   'cri': 'table-danger'
 }
+function getDueClass(row) {
+  const diff = moment(row.due) - moment()
+  return diff < 0 ? 'table-danger' : diff < WARN_THRESHOLD ? 'table-warning' : null
+}
 
 export default {
   props: ['query', 'cfg'],
+  filters: { priority, state },
   methods: {
     rowClass: function (row) {
       return prioClasses[row.prio] || ''
+    },
+    dueClass: function (row) {
+      return row.due ? getDueClass(row) : null
     }
   },
   components: { Actions },
@@ -19,13 +29,13 @@ export default {
   <ACListView :query="query" :cfg="cfg">
     <template v-slot:default="{ items, fields }">
 
-      <tr v-for="row,rowidx in items" :key="rowidx" :class="rowClass(row)">
+      <tr v-for="row,rowidx in items" :key="rowidx">
         <td>{{ row.id }}</td>
         <td>{{ row.name }}</td>
-        <td>{{ row.due | date }}</td>
-        <td>{{ row.prio }}</td>
+        <td :class="dueClass(row)">{{ row.due | date }}</td>
+        <td :class="rowClass(row)">{{ row.prio | priority }}</td>
         <td><NameSpan v-if="row.solver" :uid="row.solver" :cfg="cfg" /></td>
-        <td>{{ row.state }}</td>
+        <td>{{ row.state | state }}</td>
         <td>{{ row.tags }}</td>
         <Actions key="actions" :query="query" :row="row" :cfg="cfg" />
       </tr>
